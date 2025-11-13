38a645add15902eb67531b24a4e688894f0b6915
ilessall <irsalfa7@gmail.com>
2025-11-13 08:37:15 +0700

    Simpan perubahan lokal sebelum pull

--- warga/views.py
+++ warga/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Warga, Pengaduan
 from .forms import WargaForm, PengaduanForm
@@ -16,7 +16,18 @@ class WargaCreateView(CreateView):
     model = Warga
     form_class = WargaForm
     template_name = 'warga/warga_form.html'
-    success_url = reverse_lazy('warga-list')
+    success_url = reverse_lazy('warga:warga-list')
+
+class WargaUpdateView(UpdateView):
+    model = Warga
+    form_class = WargaForm
+    template_name = 'warga/warga_form.html'
+    success_url = reverse_lazy('warga:warga-list')
+
+class WargaDeleteView(DeleteView):
+    model = Warga
+    template_name = 'warga/warga_confirm_delete.html'
+    success_url = reverse_lazy('warga:warga-list')
 
 # Pengaduan Views
 class PengaduanListView(ListView):
@@ -28,4 +39,15 @@ class PengaduanCreateView(CreateView):
     model = Pengaduan
     form_class = PengaduanForm
     template_name = 'warga/pengaduan_form.html'
-    success_url = reverse_lazy('pengaduan-list')
+    success_url = reverse_lazy('warga:pengaduan-list')
+
+class PengaduanUpdateView(UpdateView):
+    model = Pengaduan
+    form_class = PengaduanForm
+    template_name = 'warga/pengaduan_form.html'
+    success_url = reverse_lazy('warga:pengaduan-list')
+
+class PengaduanDeleteView(DeleteView):
+    model = Pengaduan
+    template_name = 'warga/pengaduan_confirm_delete.html'
+    success_url = reverse_lazy('warga:pengaduan-list')
